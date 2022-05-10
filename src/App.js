8f1823429548e9e8c3b7bc1ecf0f0e4c6e9b1e1d
João Francisco Reis <joaofranciscoreis13@outlook.com>
2022-05-11 00:23:55 +0100

    10/05

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Hi</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,62 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faUsers,
+  faBriefcase,
+  faMessage,
+  faBell,
+  faListDots,
+} from "@fortawesome/free-solid-svg-icons";
+import logo from "../images/logo.png";
+import avatar from "../images/avatar.png";
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <div className="logo">
+        <a href="/">
+          <img src={logo} height="35px" width="35px" alt="linkedin logo"></img>
+        </a>
+      </div>
+      <div className="search">
+        <input
+          className="search-box"
+          placeholder="🔎 Search for jobs, skills, companies..."
+        ></input>
+      </div>
+      <div className="nav-btns">
+        <a href="/" className="nav-btn">
+          <FontAwesomeIcon icon={faHouse} />{" "}
+        </a>
+
+        <a href="/" className="nav-btn">
+          <FontAwesomeIcon icon={faUsers} />
+        </a>
+        <a href="/" className="nav-btn">
+          <FontAwesomeIcon icon={faBriefcase} />
+        </a>
+        <a href="/" className="nav-btn">
+          <FontAwesomeIcon icon={faMessage} />
+        </a>
+        <a href="/" className="nav-btn">
+          <FontAwesomeIcon icon={faBell} />
+        </a>
+        <div className="avatar-box">
+          <a href="/">
+            <img className="avatar" src={avatar} alt="User avatar"></img>
+          </a>
+        </div>
+        <a href="/" className="nav-btn">
+          <FontAwesomeIcon icon={faListDots} />
+        </a>
+        <div className="nav-ad-box">
+          <a href="/" className="nav-ad">
+            <p>Premium</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
